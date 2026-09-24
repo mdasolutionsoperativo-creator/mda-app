bb2a19da6562ef301492152670aa1c76b7a5b039
mdasolutionsoperativo-creator <mdasolutions.operativo@gmail.com>
2026-09-24 16:42:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,34 @@
-const CACHE = 'mda-v5';
-const ASSETS = ['/mda-app/', '/mda-app/index.html'];
+const CACHE_NAME = 'mda-cache-v3';
+const URLS = [
+  '/mda-app/',
+  '/mda-app/mda-app.html',
+  '/mda-app/manifest.json',
+  '/mda-app/icon-192.png',
+  '/mda-app/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(URLS))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ).then(() => self.clients.claim()));
-});
-
-self.addEventListener('message', event => {
-  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(response => {
-        const clone = response.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return response;
-      })
-      .catch(() => caches.match(e.request).then(r => r || caches.match('/mda-app/index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
+});
+
+self.addEventListener('message', e => {
+  if (e.data?.type === 'SKIP_WAITING') self.skipWaiting();
 });
